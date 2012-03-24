1346e05d6b4e305485a72ebee2d3de453c19c000
Sanjay Menakuru <sanjay@athinkingape.com>
2012-03-24 01:28:44 -0700

    added shell of server (mainly just copied from root binary for now)

---
+++ server/server.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"runtime/pprof"
+
+	"github.com/athinkingape/athena/trie"
+)
+
+// command line flags
+var (
+	file       = flag.String("file", "", "the file to read trie data")
+	memprofile = flag.String("memprofile", "", "write memory profile to this file")
+)
+
+func main() {
+	flag.Parse()
+	t := &trie.Tree{}
+
+	f, err := os.Open(*file)
+	if err != nil {
+		log.Fatalf("os.Open error: %v", err)
+	}
+
+	reader := bufio.NewReader(f)
+	line, isPrefix, err := reader.ReadLine()
+
+	for err == nil {
+		if isPrefix {
+			log.Fatalf("line bigger than buffer")
+		}
+
+		t.Add(string(line))
+
+		line, isPrefix, err = reader.ReadLine()
+	}
+
+	runtime.GC()
+
+	if *memprofile != "" {
+		f, err := os.Create(*memprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.WriteHeapProfile(f)
+		f.Close()
+	}
+	fmt.Fprintf(os.Stdout, "Done building ternary search tree\n")
+}
